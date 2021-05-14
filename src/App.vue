2a83719aa08b3f33227c22ab748dea737c7e0b6e
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-14 13:51:33 +0200

    Installed Bootstrap package and modified App.vue file to create beginning content of app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h4 class="bg-success text-dark text-center p-2">
+      Technology stack that Linux Kyrios wish to master
+    </h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Technology stack</div>
+        <div class="col-2 font-weight-bold">Does Linux Kyrios know this stack?</div>
+      </div>
+      <div class="row" v-for="s in stack" v-bind:key="s.action">
+        <div class="col">{{s.action}}</div>
+        <div class="col-2">{{s.done}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  //name: 'App',
+  data: function() {
+    return {
+      stack: [{ action: "JavasScript", done: true },
+              { action: "Vue.js", done: false },
+              { action: "React", done: false },
+              { action: "Angular", done: false },
+              { action: ".NET Core MVC", done: false },
+              { action: "C#", done: false }]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
