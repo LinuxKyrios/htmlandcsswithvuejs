b04544ad4f4531c49839d1b43f5511487f3fd992
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-14 20:59:54 +0200

    Applied Bootstrap styling to App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <div id="app">
-    <h4 class="bg-success text-dark text-center p-2">
+  <div id="app" >
+    <h4 class="bg-success text-dark text-center m-1 p-2">
       Technology stack that Linux Kyrios wish to master
     </h4>
-    <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Technology stack</div>
-        <div class="col-2 font-weight-bold">Does Linux Kyrios know this stack?</div>
-      </div>
-      <div class="row" v-for="s in stack" v-bind:key="s.action">
-        <div class="col">{{s.action}}</div>
-        <div class="col-2">{{s.done}}</div>
-      </div>
-    </div>
+    <table class="table table-striped table-bordered table-sm">
+      <thead>
+      <tr><th>Technology stack</th><th>Does Linux Kyrios know this stack?</th></tr>
+      </thead>
+      <tbody>
+        <tr v-for="s in stack" v-bind:key="s.action">
+          <td>{{s.action}}</td>
+          <td>{{s.done}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
